beff58aeb990992ec9fa4483e34f3026fbd4afdf
Henri Cazottes <henri.cazottes@logilab.fr>
2020-04-03 19:35:11 +0200

    Add notfication

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Notifications } from 'expo';
 import axios from 'axios';
+
+const getLocalNotification = (isAvailable) => ({
+  title: `Leclerc Drive Status ${isAvailable ? '🟢' : '🔴'}`,
+  ios: { // (optional) (object) — notification configuration specific to iOS.
+    sound: true // (optional) (boolean) — if true, play a sound. Default: false.
+  },
+android: // (optional) (object) — notification configuration specific to Android.
+  {
+    sound: true, // (optional) (boolean) — if true, play a sound. Default: false.
+    icon: './assets/available.png',
+    priority: 'high',
+    vibrate: true,
+    sticky: true,
+  }
+});
 
 function fetchPage(setAvailable, setLoading) {
   setLoading(true);
@@ -11,23 +26,34 @@ function fetchPage(setAvailable, setLoading) {
   .then(response => {
     const aspNetRegExp = new RegExp(/id="aspnetForm"/g);
     const isAvailable = (response.data.match(aspNetRegExp) !== null);
+    triggerNotification(isAvailable);
     setAvailable(isAvailable);
   })
   .catch(err => console.error(err))
   .finally(() => setLoading(false))
 }
 
+function triggerNotification(isAvailable) {
+  Notifications.dismissAllNotificationsAsync()
+  .then(() => {
+    Notifications.presentLocalNotificationAsync(
+      getLocalNotification(isAvailable),
+    );
+  })
+}
+
 export default function App() {
   const [available, setAvailable] = useState(false);
   const [loading, setLoading] = useState(false);
-
   const statusText = `Status: ${available ? 'Available' : 'Not Available'}`;
 
   return (
     <View style={styles.container}>
       <Button
         title='Reload'
-        onPress={() => fetchPage(setAvailable, setLoading)}
+        onPress={() => {
+          fetchPage(setAvailable, setLoading);
+        }}
       >
         Reload
       </Button>
